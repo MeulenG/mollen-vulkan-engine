3b8971ba613875a0a0e0463f73c8f5e692080fc9
Oliver Meulengracht <Olivermeulengracht@protonmail.com>
2026-04-04 15:44:04 +0200

    engine/resources: Buffer implementation

---
+++ engine/resources/buffer.cpp
@@ -0,0 +1,73 @@
+#include "buffer.h"
+
+#include <cstring>
+#include <stdexcept>
+
+namespace mve {
+
+Buffer::Buffer(
+    Device& device,
+    vk::DeviceSize size,
+    vk::BufferUsageFlags usage,
+    vk::MemoryPropertyFlags properties)
+    : device_{device}, size_{size} {
+
+    buffer_ = device_.device().createBuffer({{}, size, usage});
+
+    auto mem_requirements = buffer_.getMemoryRequirements();
+
+    memory_ = device_.device().allocateMemory({
+        mem_requirements.size,
+        device_.findMemoryType(mem_requirements.memoryTypeBits, properties)
+    });
+
+    buffer_.bindMemory(*memory_, 0);
+}
+
+void* Buffer::map() {
+    if (!mapped_) {
+        mapped_ = memory_.mapMemory(0, size_);
+    }
+    return mapped_;
+}
+
+void Buffer::unmap() {
+    if (mapped_) {
+        memory_.unmapMemory();
+        mapped_ = nullptr;
+    }
+}
+
+void Buffer::write(const void* data, vk::DeviceSize size) {
+    void* dest = map();
+    std::memcpy(dest, data, static_cast<size_t>(size));
+    unmap();
+}
+
+Buffer Buffer::createWithStaging(
+    Device& device,
+    const void* data,
+    vk::DeviceSize size,
+    vk::BufferUsageFlags usage) {
+
+    Buffer staging{
+        device, size,
+        vk::BufferUsageFlagBits::eTransferSrc,
+        vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent
+    };
+    staging.write(data, size);
+
+    Buffer gpu_buffer{
+        device, size,
+        usage | vk::BufferUsageFlagBits::eTransferDst,
+        vk::MemoryPropertyFlagBits::eDeviceLocal
+    };
+
+    auto cmd = device.beginSingleTimeCommands();
+    cmd.copyBuffer(*staging.buffer(), *gpu_buffer.buffer(), vk::BufferCopy{0, 0, size});
+    device.endSingleTimeCommands(std::move(cmd));
+
+    return gpu_buffer;
+}
+
+} // namespace mve
--- engine/resources/buffer.h
+++ engine/resources/buffer.h
@@ -7,8 +7,36 @@ namespace mve {
 
 class Buffer {
 public:
+    Buffer(
+        Device& device,
+        vk::DeviceSize size,
+        vk::BufferUsageFlags usage,
+        vk::MemoryPropertyFlags properties);
+
     Buffer(const Buffer&) = delete;
     Buffer& operator=(const Buffer&) = delete;
+    Buffer(Buffer&&) = default;
+    Buffer& operator=(Buffer&&) = default;
+
+    const vk::raii::Buffer& buffer() const { return buffer_; }
+    vk::DeviceSize size() const { return size_; }
+
+    void* map();
+    void unmap();
+    void write(const void* data, vk::DeviceSize size);
+
+    static Buffer createWithStaging(
+        Device& device,
+        const void* data,
+        vk::DeviceSize size,
+        vk::BufferUsageFlags usage);
+
+private:
+    Device& device_;
+    vk::DeviceSize size_;
+    vk::raii::Buffer buffer_{nullptr};
+    vk::raii::DeviceMemory memory_{nullptr};
+    void* mapped_ = nullptr;
 };
 
 } // namespace mve
